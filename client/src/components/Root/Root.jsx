2cfdaab76627f8f1756cc4046ec6f9a2c5dfd414
Yann Darwish <yann.darwish@gmail.com>
2022-11-25 10:35:08 +0100

    root styling

--- client/src/components/Root/Root.jsx
+++ client/src/components/Root/Root.jsx
@@ -1,14 +1,22 @@
-import { Text, View } from "react-native"
+import { Text, View, StyleSheet } from "react-native"
 import { Outlet } from "react-router-native"
-import Sidebar from "../Sidebar/Sidebar"
+import Sidebar from "../Sidebar/Sidebar.component"
 
 const Root = () => {
     return (
-        <View>
+        <View style={styles.container}>
             <Sidebar />
             <Outlet />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "white",
+        flexDirection: "row",
+	},
+})
 
 export default Root
