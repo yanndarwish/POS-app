9c2e485d9bbcb6f67eb1e5426ab70c1223241013
Yann Darwish <yann.darwish@gmail.com>
2022-11-25 15:10:05 +0100

    profile clean up

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,11 +1,11 @@
 import { Text, View } from "react-native"
 
 const Profile = () => {
-    return (
-        <View>
-            <Text>Profile</Text>
-        </View>
-    )
+	return (
+		<View>
+			<Text>Profile</Text>
+		</View>
+	)
 }
 
 export default Profile
