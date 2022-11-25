d047c245b5dc9beeb692da335b14e45a91843394
Yann Darwish <yann.darwish@gmail.com>
2022-11-25 16:44:34 +0100

    login slice and reducer

---
+++ client/src/redux/features/login.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+	loggedIn: false,
+}
+
+const loginSlice = createSlice({
+	name: "login",
+	initialState,
+	reducers: {
+		login: (state) => {
+			state.loggedIn === true
+		},
+		logout: (state) => {
+			state.loggedIn === false
+		}
+	},
+})
+
+export const { login, logout } = loginSlice.actions
+export default loginSlice.reducer
--- client/src/redux/reducer/reducer.js
+++ client/src/redux/reducer/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import theme from "../features/theme";
+import login from "../features/login";
 
 const rootReducer = combineReducers({
     theme,
+    login
 })
 
 export default rootReducer
