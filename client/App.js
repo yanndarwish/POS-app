b4b50266ede3f10e679721bf89a9c93cda2640d7
Yann Darwish <yann.darwish@gmail.com>
2022-11-25 15:10:25 +0100

    redux setup

--- client/App.js
+++ client/App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from "expo-status-bar"
 import { StyleSheet, View } from "react-native"
 import { NativeRouter, Route, Routes } from "react-router-native"
+import { Provider } from "react-redux"
+import store from "./src/redux/store/store"
 import Root from "./src/components/Root/Root"
 import Dashboard from "./src/pages/Dashboard/Dashboard"
 import Help from "./src/pages/Help/Help"
@@ -11,27 +13,27 @@ import Logout from "./src/pages/Logout/Logout"
 import Pos from "./src/pages/Pos/Pos"
 import Profile from "./src/pages/Profile/Profile"
 
-
 export default function App() {
-
 	return (
-		<NativeRouter>
-			<View style={styles.container}>
-				<StatusBar style="auto" />
-				<Routes>
-					<Route path="/" element={<Root />}>
-						<Route path="/" element={<Home />} />
-						<Route path="login" element={<Login />} />
-						<Route path="pos" element={<Pos />} />
-						<Route path="dashboard" element={<Dashboard />} />
-						<Route path="inventory" element={<Inventory />} />
-						<Route path="profile" element={<Profile />} />
-						<Route path="help" element={<Help />} />
-						<Route path="logout" element={<Logout />} />
-					</Route>
-				</Routes>
-			</View>
-		</NativeRouter>
+		<Provider store={store}>
+			<NativeRouter>
+				<View style={styles.container}>
+					<StatusBar style="auto" />
+					<Routes>
+						<Route path="/" element={<Root />}>
+							<Route path="/" element={<Home />} />
+							<Route path="login" element={<Login />} />
+							<Route path="pos" element={<Pos />} />
+							<Route path="dashboard" element={<Dashboard />} />
+							<Route path="inventory" element={<Inventory />} />
+							<Route path="profile" element={<Profile />} />
+							<Route path="help" element={<Help />} />
+							<Route path="logout" element={<Logout />} />
+						</Route>
+					</Routes>
+				</View>
+			</NativeRouter>
+		</Provider>
 	)
 }
 
@@ -39,8 +41,8 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: "red",
-    height:"100%",
-    width:"100%",
-    marginTop: 24
+		height: "100%",
+		width: "100%",
+		marginTop: 24,
 	},
 })
---
+++ client/src/redux/reducer/reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "@reduxjs/toolkit";
+import theme from "../features/theme";
+
+const rootReducer = combineReducers({
+    theme,
+})
+
+export default rootReducer
