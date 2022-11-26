d2bd45d70b2d461752b7aae01b6d903a5c1b1d44
Yann Darwish <yann.darwish@gmail.com>
2022-11-26 10:47:15 +0100

    basic shared main container style

--- client/src/pages/Pos/Pos.jsx
+++ client/src/pages/Pos/Pos.jsx
@@ -1,11 +1,63 @@
 import { Text, View } from "react-native"
+import { useSelector } from "react-redux"
+import Button from "../../components/Button/Button.component"
+import Input from "../../components/Input/Input.component"
+import { shared } from "../../styles/common.styles"
 
 const Pos = () => {
-    return (
-        <View>
-            <Text>Pos</Text>
-        </View>
-    )
+	const theme = useSelector((state) => state.theme.theme)
+	return (
+		<View
+			style={[
+				shared.container,
+				theme === "dark" ? shared.darkContainer : shared.lightContainer,
+			]}
+		>
+			<View style={[shared.header]}>
+				<Text
+					style={[
+						shared.pageTitle,
+						theme === "dark" ? shared.darkText : shared.lightText,
+					]}
+				>
+					Pos
+				</Text>
+			</View>
+			<View style={[shared.searchSection]}>
+				<Input theme={theme} style={{ width: 300 }} />
+				<Button color="purple" title="No Barcode" />
+			</View>
+			<View
+				style={[
+					shared.body,
+					theme === "dark" ? shared.darkBody : shared.lightBody,
+				]}
+			>
+				<View style={[shared.bodyHeader]}>
+					<Text
+						style={[
+							shared.bodyTitle,
+							theme === "dark" ? shared.darkText : shared.lightText,
+						]}
+					>
+						Panier
+					</Text>
+				</View>
+				<View style={[shared.bodyContent]}></View>
+
+				<View style={[shared.bodyFooter]}>
+					<View style={[shared.bodyFooterLeft]}>
+						<Button color="purple" title="Receipt" />
+						<Button color="purple" title="Drawer" />
+						<Button color="purple" title="Discount" />
+					</View>
+					<View style={[shared.bodyFooterRight]}>
+						<Button color="green" title="Continue to Payment" />
+					</View>
+				</View>
+			</View>
+		</View>
+	)
 }
 
 export default Pos
--- client/src/styles/common.styles.js
+++ client/src/styles/common.styles.js
@@ -1,4 +1,8 @@
-export default {
+import { StyleSheet } from "react-native"
+import dark from "./dark.theme.styles"
+import light from "./light.theme.styles"
+
+const style = {
 	// padding
 	PADDING_XS: 6,
 	PADDING_S: 12,
@@ -13,13 +17,13 @@ export default {
 	MARGIN_XL: 96,
 	// gap
 	GAP: 24,
-    // font-size
-    FONT_PAGE_TITLE: 52,
-    FONT_SECTION_TITLE:36,
-    FONT_ARTICLE_TITLE: 24,
-    FONT_SUBTITLE: 24,
-    FONT_LABEL: 16,
-    FONT_BODY: 20,
+	// font-size
+	FONT_PAGE_TITLE: 52,
+	FONT_SECTION_TITLE: 36,
+	FONT_ARTICLE_TITLE: 24,
+	FONT_SUBTITLE: 24,
+	FONT_LABEL: 16,
+	FONT_BODY: 20,
 	// border-radius
 	BORDER_RADIUS_S: 4,
 	BORDER_RADIUS_M: 6,
@@ -29,3 +33,58 @@ export default {
 	CLR_WARNING: "#FAAE2B",
 	CLR_DANGER: "#EF4565",
 }
+
+export const shared = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: style.PADDING_L,
+		backgroundColor: "blue",
+	},
+	lightContainer: {
+		backgroundColor: light.COLOR_BG_PRIMARY,
+		color: light.COLOR_TEXT,
+	},
+	darkContainer: {
+		backgroundColor: dark.COLOR_BG_PRIMARY,
+		color: dark.COLOR_TEXT,
+	},
+	header: {},
+	pageTitle: {
+		fontSize: style.FONT_PAGE_TITLE,
+		fontWeight: "bold",
+		marginBottom: style.GAP * 2,
+	},
+	lightText: {
+		color: light.COLOR_TEXT,
+	},
+	darkText: {
+		color: dark.COLOR_TEXT,
+	},
+	searchSection: {
+		flexDirection: "row",
+	},
+	body: {
+		padding: style.PADDING_M,
+	},
+	lightBody: {
+		backgroundColor: light.COLOR_BG_SECONDARY,
+	},
+	darkBody: {
+		backgroundColor: dark.COLOR_BG_SECONDARY,
+	},
+	bodyHeader: {
+		marginBottom: style.GAP,
+	},
+	bodyTitle: {
+		fontSize: style.FONT_SECTION_TITLE,
+		fontWeight: "bold",
+	},
+	bodyFooter: {
+		flexDirection: "row",
+	},
+	bodyFooterLeft: {
+		flexDirection: "row",
+	},
+})
+
+export default style
